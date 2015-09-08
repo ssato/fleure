ab293b875d70430f17c12344c4af58dabe9d6910
Satoru SATOH <satoru.satoh@gmail.com>
2015-09-09 01:57:09 +0900

    add basic packaging stuff

---
+++ README.rst
@@ -0,0 +1,16 @@
+=========
+fleure
+=========
+
+fleure, a successor of rk-updateinfo (rpmkit.updateinfo cli), is a package
+level static analysis tool for systems running RPM-based linux distributions
+such like RHEL (primary target) and Fedora.
+
+fleure loads RPM database files, tries to fetch updateinfo data from yum repos
+and do some analysis based on these data.
+
+- Home: https://github.com/ssato/fleure
+- Author: Satoru SATOH <ssato at redhat.com>
+- License: AGPLv3+ and GPLv3+
+
+.. vim:sw=2:ts=2:et:
---
+++ pkg/update-po.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+set -ex
+
+pkgname="fleure"
+
+curdir=${0%/*}
+topdir=${curdir}/../
+podir=${topdir}/po
+localedir=${topdir}/${pkgname}/locale
+
+# TODO: switch from pygettext to xgettext
+${curdir}/pygettext.py -d ${pkgname} -p ${podir} $(find ${topdir}/${pkgname} -name '*.py' | grep -v test)
+#xgettext --from-code=utf-8 -L python -d ${pkgname} -p ${podir} $(find ${topdir}/${pkgname} -name '*.py' | grep -v test)
+
+for po in ${podir}/*.po; do
+    t=${po##*/}
+    lang=${t/.po/}
+    locdir=${localedir}/${lang}/LC_MESSAGES
+
+    test -d ${locdir} || mkdir -p ${locdir}
+
+    msgmerge  --update ${po} ${podir}/${pkgname}.pot
+    msgfmt -v ${po} -o ${locdir}/${pkgname}.mo
+done
