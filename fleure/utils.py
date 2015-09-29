6a06219c3a1cf4e77aed4473302c1b4be5201113
Satoru SATOH <satoru.satoh@gmail.com>
2015-09-29 12:32:28 +0900

    remove a log message on dropping write acces perm of RPM DB files

--- fleure/utils.py
+++ fleure/utils.py
@@ -221,7 +221,6 @@ def check_rpmdb_root(root, readonly=True, system=False,
             LOG.info("RPM DB %s looks missing", dbn)
 
         if readonly and os.access(dbpath, os.W_OK) and not system:
-            LOG.debug("Drop write access perm from %s ", dbn)
             os.chmod(dbpath, 0o444)
 
     return True
