4325dd9aadf806e43069208c10ce5d60fd649416
Satoru SATOH <satoru.satoh@gmail.com>
2015-12-07 11:38:42 +0900

    fix: fix some pep8 errors (line too long, etc.)

--- tests/line_profile.py
+++ tests/line_profile.py
@@ -19,6 +19,23 @@ import fleure.datasets
 import fleure.main
 
 
+TARGETS = (fleure.main.main,
+           fleure.main.dump_results,
+           fleure.main.prepare,
+           fleure.main.analyze,
+           fleure.main.archive_report,
+           fleure.analysis.list_latest_errata_by_updates,
+           fleure.analysis.list_updates_from_errata,
+           fleure.analysis.errata_of_keywords_g,
+           fleure.analysis.list_update_errata_pairs,
+           fleure.analysis.list_updates_by_num_of_errata,
+           fleure.analysis.analyze_rhsa,
+           fleure.analysis.analyze_rhba,
+           fleure.analysis.analyze_errata,
+           fleure.analysis.mk_overview_dataset,
+           fleure.datasets.make_dataset)
+
+
 def prof_main():
     """main.
     """
@@ -28,21 +45,7 @@ def prof_main():
     cnf = dict(workdir=tmpdir, repos=["rhel-6-server-rpms"], verbosity=2,
                period=["2015-01-01", "2015-11-11"], archive=True)
 
-    prof = line_profiler.LineProfiler(fleure.main.main,
-                                      fleure.main.dump_results,
-                                      fleure.main.prepare,
-                                      fleure.main.analyze,
-                                      fleure.main.archive_report,
-                                      fleure.analysis.list_latest_errata_by_updates,
-                                      fleure.analysis.list_updates_from_errata,
-                                      fleure.analysis.errata_of_keywords_g,
-                                      fleure.analysis.list_update_errata_pairs,
-                                      fleure.analysis.list_updates_by_num_of_errata,
-                                      fleure.analysis.analyze_rhsa,
-                                      fleure.analysis.analyze_rhba,
-                                      fleure.analysis.analyze_errata,
-                                      fleure.analysis.mk_overview_dataset,
-                                      fleure.datasets.make_dataset)
+    prof = line_profiler.LineProfiler(*TARGETS)
     prof.runcall(fleure.main.main, root_or_arc_path, **cnf)
     prof.print_stats()
     prof.dump_stats(os.path.join(tmpdir, "test.prof"))
