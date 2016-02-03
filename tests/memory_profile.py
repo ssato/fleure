adc0e39eb0749a12aa17d7ae01435dec8e42a6a6
Satoru SATOH <satoru.satoh@gmail.com>
2016-02-03 15:51:12 +0900

    enhancement: add memory profile test case and its sample log

---
+++ tests/memory_profile.py
@@ -0,0 +1,60 @@
+#
+# Copyright (C) 2015 Satoru SATOH <ssato@redhat.com>
+# License: MIT
+#
+"""profiling fleure with line_profiler and memory_profiler.
+"""
+from __future__ import absolute_import
+
+import os.path
+import os
+import sys
+import tempfile
+
+try:
+    import line_profiler
+    import memory_profiler
+except ImportError:
+    sys.exit(0)
+
+import fleure.analysis
+import fleure.datasets
+import fleure.main
+
+
+TARGETS = (fleure.main.main,
+           # fleure.main.analyze_and_save_results,
+           fleure.main.prepare,
+           fleure.main.analyze,
+           fleure.main.archive_report,
+           fleure.analysis.list_latest_errata_by_updates,
+           fleure.analysis.list_updates_from_errata,
+           fleure.analysis.errata_of_keywords_g,
+           fleure.analysis.list_update_errata_pairs,
+           fleure.analysis.list_updates_by_num_of_errata,
+           fleure.analysis.analyze_rhsa,
+           fleure.analysis.analyze_rhba,
+           fleure.analysis.analyze_errata,
+           fleure.analysis.mk_overview_dataset,
+           fleure.datasets.make_dataset)
+
+
+def prof_main():
+    """main.
+    """
+    tmpdir = tempfile.mkdtemp(dir="/tmp", prefix="fleure-tests-")
+    root_or_arc_path = os.path.join(os.path.dirname(__file__),
+                                    "rhel-6-client-1_var_lib_rpm.tar.xz")
+    cnf = dict(workdir=tmpdir, repos=["rhel-6-server-rpms"], verbosity=2,
+               period=["2015-01-01", "2015-11-11"], archive=True)
+
+    lprof = line_profiler.LineProfiler(*TARGETS)
+    lprof.runcall(fleure.main.main, root_or_arc_path, **cnf)
+    lprof.print_stats()
+    lprof.dump_stats(os.path.join(tmpdir, "test.prof"))
+
+
+if __name__ == '__main__':
+    prof_main()
+
+# vim:sw=4:ts=4:et:
