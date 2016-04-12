2bbf2b2bc57703ba6123666fae6ee08c2e6fae8f
Satoru SATOH <satoru.satoh@gmail.com>
2016-04-12 12:26:19 +0900

    enhancement: add test cases of overhauled fleure.package.inspect_origin

--- fleure/tests/package.py
+++ fleure/tests/package.py
@@ -2,26 +2,36 @@
 # Copyright (C) 2016 Satoru SATOH <ssato at redhat.com>
 # License: AGPLv3+
 #
-# pylint: disable=missing-docstring
+# pylint: disable=missing-docstring,invalid-name
 from __future__ import absolute_import
 
 import unittest
 import fleure.package as TT
+from fleure.tests.common import dicts_equal
 
 
 class Test00(unittest.TestCase):
 
-    def test_20_may_be_rebuilt(self):
-        vendors = ("Red Hat, Inc.", "ZABBIX-JP", "Example, Inc.")
-        self.assertFalse(TT.may_be_rebuilt(vendors[0],
-                                           "abc.builder.redhat.com"))
-        self.assertTrue(TT.may_be_rebuilt(vendors[0], "localhost"))
-        self.assertTrue(TT.may_be_rebuilt(vendors[0], "localhost.localdomain"))
-        self.assertFalse(TT.may_be_rebuilt(vendors[1],
-                                           "abc.builder.redhat.com"))
-        self.assertFalse(TT.may_be_rebuilt(vendors[2],
-                                           "abc.builder.redhat.com"))
-        self.assertFalse(TT.may_be_rebuilt(vendors[2],
-                                           "localhost.localdomain"))
+    def test_10_inspect_origin__genuine(self):
+        origin = TT.inspect_origin("bash", TT.VENDOR_RH, "builder.redhat.com")
+        ref = dict(origin="redhat", rebuilt=False, replaced=False)
+        self.assertTrue(dicts_equal(origin, ref))
+
+    def test_12_inspect_origin__genuine_other_vendor(self):
+        origin = TT.inspect_origin("crash-trace-command", "Fujitsu Limited",
+                                   "builder.redhat.com")
+        ref = dict(origin="redhat", rebuilt=False, replaced=False)
+        self.assertTrue(dicts_equal(origin, ref))
+
+    def test_14_inspect_origin__genuine_other_vendor_w_extras(self):
+        origin = TT.inspect_origin("crash-trace-command", "Fujitsu Limited",
+                                   "builder.redhat.com", extra_names=[])
+        ref = dict(origin="redhat", rebuilt=False, replaced=False)
+        self.assertTrue(dicts_equal(origin, ref))
+
+    def test_16_inspect_origin__rebuilt(self):
+        origin = TT.inspect_origin("bash", TT.VENDOR_RH, "localhost")
+        ref = dict(origin="redhat", rebuilt=True, replaced=False)
+        self.assertTrue(dicts_equal(origin, ref), origin)
 
 # vim:sw=4:ts=4:et:
