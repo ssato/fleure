be47e1f7c911498887e18868415dbfb71540ba2c
Satoru SATOH <satoru.satoh@gmail.com>
2016-04-19 15:53:20 +0900

    enhancement: add a test cases to add some packages in fleure.models.tests.package

--- fleure/models/tests/package.py
+++ fleure/models/tests/package.py
@@ -5,6 +5,7 @@
 # pylint: disable=missing-docstring,invalid-name,no-member
 from __future__ import absolute_import
 
+import itertools
 import unittest
 
 import fleure.models.package as TT
@@ -75,5 +76,22 @@ class Test20(fleure.models.tests.common.TestsWithSession):
         pkgs = self.session.query(TT.Package).all()
         self.assertEquals(len(pkgs), 1)
         self.assertEquals(pkgs[0].nevra, "kernel 0:2.6.38.8-32 x86_64")
+        self.assertEquals(pkgs[0].rebuilt, False)
+        self.assertEquals(pkgs[0].from_others, False)
+
+    def test_20_add_some(self):
+        ref = [TT.Package("nss", "3.16.1-4", "1.el6_5", "x86_64", "0"),
+               TT.Package("nss-util", "3.16.1-4", "1.el6_5", "x86_64", "0"),
+               TT.Package("nspr", "4.10.6", "1.el6_5", "x86_64", "0")]
+
+        self.session.add_all(ref)
+        self.session.commit()
+
+        pkgs = self.session.query(TT.Package).all()
+        self.assertEquals(len(pkgs), len(ref))
+        for pkg_ref, pkg in itertools.izip_longest(ref, pkgs):
+            self.assertEquals(pkg.nevra, pkg_ref.nevra)
+            self.assertEquals(pkg.rebuilt, pkg_ref.rebuilt)
+            self.assertEquals(pkg.from_others, pkg_ref.from_others)
 
 # vim:sw=4:ts=4:et:
