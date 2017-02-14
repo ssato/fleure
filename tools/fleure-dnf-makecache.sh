8df81cbc59093e74e08c4cc6854874967cd300f1
Satoru SATOH <satoru.satoh@gmail.com>
2017-02-15 03:04:43 +0900

    enhancement: add files to update basic yum metadata cache periodically

---
+++ tools/fleure-dnf-makecache.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+#
+# A tiny script wrapping 'yum/dnf makecache'.
+#
+set -e
+
+# Defaults:
+DNF=$(which dnf || which yum) # Prefer dnf instead of yum
+REPOS="
+rhel-6-server-rpms
+rhel-6-server-optional-rpms
+rhel-6-server-extras-rpms
+rhel-7-server-rpms
+rhel-7-server-optional-rpms
+rhel-7-server-extras-rpms
+"
+CONFDIR=/etc/fleure/db/
+
+# main:
+test -d ${CONFDIR} && (for f in ${CONFDIR}/*.conf; do test -f $f && source $f; done)
+
+REPO_OPTS="--disablerepo '*'"
+for repo in ${REPOS:?}; do REPO_OPTS="${REPO_OPTS} --enablerepo ${repo}"; done
+
+${DNF:?} makecache ${REPO_OPTS}
+
+# vim:sw=4:ts=4:et:
