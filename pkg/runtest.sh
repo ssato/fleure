4a87dab48cf9a718bcb4ca01d416d17134a2b81a
Satoru SATOH <satoru.satoh@gmail.com>
2019-05-05 14:33:16 +0900

    change: add support of python version swtich

--- pkg/runtest.sh
+++ pkg/runtest.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 set -ex
 
-pip-3 install -r pkg/test_requirements.txt
-flake8-3 --doctests fleure tests
+PY_VER=${1:-3}
+
+pip-${PY_VER:?} install -r pkg/test_requirements.txt
+flake8-${PY_VER:?} --doctests fleure tests
 # .. note::
 #    pylint may exit w/ !0 code even if there are not errors but warnings.
-pylint-3 --disable=invalid-name,locally-disabled fleure || :
-python3 -m nose -v --with-doctest --all-modules --where fleure --with-coverage --cover-tests
+pylint-${PY_VER:?} --disable=invalid-name,locally-disabled fleure || :
+python${PY_VER:?} -m nose -v --with-doctest --all-modules --where fleure --with-coverage --cover-tests
